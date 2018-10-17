9d2e81e1bb2e29b50aa1707295c20d1ea8881a90
wellyoung <804110392@qq.com>
2018-10-17 19:34:08 +0800

    First Commit

---
+++ Ex_1017/Ex_1017/main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  Ex_1017
+//
+//  Created by 魏阳 on 2018/10/17.
+//  Copyright © 2018 魏阳. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int sum = 0, value = 0;
+    while (cin >> value)
+    {
+        sum += value;
+    }
+    cout << "sum is"<< sum << endl;
+}
